5ad38ce919013cd51994e6e42d4328e701984002
PavithiraRajen <108916815+PavithiraRajen@users.noreply.github.com>
2022-07-12 11:46:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import request
 from flask import jsonify
 import re
 import os
-from sklearn.metrics.pairwise import cosine_similarity
+#from sklearn.metrics.pairwise import cosine_similarity
 import tensorflow as tf
 import tensorflow_hub as hub
 
