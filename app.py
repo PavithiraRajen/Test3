cbbded448203df4b05c63cd414c23c99caf7b5b6
PavithiraRajen <108916815+PavithiraRajen@users.noreply.github.com>
2022-07-12 12:18:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask
+from flask import request
+from flask import jsonify
+import re
+import os
+from sklearn.metrics.pairwise import cosine_similarity
+import tensorflow as tf
+import tensorflow_hub as hub
+
+
+#model = hub.load(r"https://tfhub.dev/google/universal-sentence-encoder-large/5")
 
 
 app = Flask(__name__)
